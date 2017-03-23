4d5192f20f1304a13d7bd85214d547610e6bc784
yeslhh13 <gowonkid@naver.com>
2017-03-23 18:17:05 +0900

    removed unused import declaration

--- app/src/main/java/com/example/android/myinventory/data/ProductDBHelper.java
+++ app/src/main/java/com/example/android/myinventory/data/ProductDBHelper.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.io.File;
-
 /**
  * Created by Kat on 2017-03-22.
  */
